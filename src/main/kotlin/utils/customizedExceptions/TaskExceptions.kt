2ff09b08ecce7ca6572cb012eaad26945db8dd44
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-27 20:08:32 +0300

    Add custom exceptions for task validation errors

---
+++ src/main/kotlin/utils/customizedExceptions/TaskExceptions.kt
@@ -0,0 +1,7 @@
+package org.baghdad.utils.customizedExceptions
+
+class TaskMissingTitleException(): Exception()
+class TaskMissingDescriptionException(): Exception()
+class TaskMissingStateIdException(): Exception()
+class TaskMissingProjectIdException(): Exception()
+class TaskMissingCreatorIdException(): Exception()
