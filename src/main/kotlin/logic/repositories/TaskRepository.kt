ff152429a5af51172dee35779ddf7b9d5098f2dd
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-27 20:08:48 +0300

    Update getTaskById method to return non-null TaskEntity

--- src/main/kotlin/logic/repositories/TaskRepository.kt
+++ src/main/kotlin/logic/repositories/TaskRepository.kt
@@ -5,7 +5,7 @@ import org.baghdad.logic.entities.TaskEntity
 interface TaskRepository {
 
     fun createTask(task: TaskEntity): Boolean
-    fun getTaskById(id: String): TaskEntity?
+    fun getTaskById(id: String): TaskEntity
     fun getTasksByProjectId(projectId: String): List<TaskEntity>
     fun updateTask(task: TaskEntity): Boolean
     fun deleteTask(id: String): Boolean
