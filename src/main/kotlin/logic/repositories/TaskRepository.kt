e2e96374bb832d20eeca19cec1b8ef1b80795817
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-27 18:34:30 +0300

    Refactor TaskRepository interface to return Boolean for task operations

--- src/main/kotlin/logic/repositories/TaskRepository.kt
+++ src/main/kotlin/logic/repositories/TaskRepository.kt
@@ -4,9 +4,9 @@ import org.baghdad.logic.entities.TaskEntity
 
 interface TaskRepository {
 
-    fun createTask(task: TaskEntity)
+    fun createTask(task: TaskEntity): Boolean
     fun getTaskById(id: String): TaskEntity?
     fun getTasksByProjectId(projectId: String): List<TaskEntity>
-    fun updateTask(task: TaskEntity)
-    fun deleteTask(id: String)
+    fun updateTask(task: TaskEntity): Boolean
+    fun deleteTask(id: String): Boolean
 }
