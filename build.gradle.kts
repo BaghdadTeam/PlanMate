a9ce0b605fb29163eef81382b21cc2cf6bd7a6a9
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-27 09:54:48 +0300

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,126 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    jacoco
+}
+group = "org.baghdad"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    // koin
+    implementation("io.insert-koin:koin-core:4.0.3")
+
+    // kotlin date time
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // kotest, assertion
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+
+    // google truth
+    testImplementation("com.google.truth:truth:1.4.2")
+
+    // mockk
+    testImplementation("io.mockk:mockk:1.14.0")
+
+    // junit params
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.4.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        csv.required.set(true)  // Enable CSV reports for additional processing if needed
+        xml.required.set(true)  // Required for coverage-diff to work
+        html.required.set(true) // Human-readable reports
+    }
+    // Customize the JaCoCo report generation task
+    dependsOn(tasks.test)
+
+    // Set up exclusions for certain packages
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/di/**") // Exclude DI package
+            exclude("**/entities/**") // Exclude entities package
+            exclude("**/generated/**") // Exclude generated code if any
+        }
+    )
+
+    // Define the source directories for Jacoco
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(layout.buildDirectory).include("jacoco/test.exec"))
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+            exclude("**/di/**")
+            exclude("**/entities/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(layout.buildDirectory).include("jacoco/test.exec"))
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "1".toBigDecimal() // 100% coverage requirement
+            }
+        }
+        rule {
+            element = "CLASS"
+            includes = listOf("org.example.*") // Adjust package name as needed
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+            limit {
+                counter = "METHOD"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+        }
+    }
+}
+
+
+tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+            exclude("**/di/**")
+            exclude("**/entities/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(layout.buildDirectory).include("jacoco/test.exec"))
+}
