521b9b4f962647361db43d56029e63d0d0f1c987
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-29 11:22:37 +0300

    Add State data source and parser for handling state entities

---
+++ src/main/kotlin/data/datasource/parser/state/StateColumns.kt
@@ -0,0 +1,8 @@
+package org.baghdad.data.datasource.parser.state
+
+object StateColumns {
+    const val ID = 0
+    const val NAME = 1
+    const val PROJECT_ID = 2
+    const val CREATOR_ID = 3
+}
