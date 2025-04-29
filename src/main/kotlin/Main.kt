b5062a4f2fce7615942d617b037a438b9c724e2b
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-29 12:03:37 +0300

    Initialize Koin for dependency injection in the main application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.baghdad
 
+import org.baghdad.data.local.ProjectDataSource
+import org.baghdad.di.appModule
+import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform.getKoin
+
 fun main() {
-    println("PlanMate application starting...")
+    startKoin {
+        modules(appModule)
+    }
 }
