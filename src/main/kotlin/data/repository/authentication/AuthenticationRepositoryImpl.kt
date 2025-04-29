f130b633c5bf4f3f6bef5e5ab37a51265361aa48
haider <haideraljawasim@gmail.com>
2025-04-30 02:33:41 +0300

    change the returend in login AuthentcationRepository from SessionEntity to UserEntity for better sparate of conecern

--- src/main/kotlin/data/repository/authentication/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repository/authentication/AuthenticationRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package org.baghdad.data.repository.authentication
 import org.baghdad.data.local.UserDataSource
 import org.baghdad.logic.model.entities.SessionEntity
+import org.baghdad.logic.model.entities.UserEntity
 import org.baghdad.logic.repositories.AuthenticationRepository
 import org.baghdad.logic.repositories.SessionRepository
 
@@ -9,7 +10,7 @@ class AuthenticationRepositoryImpl(
     private val userDataSource: UserDataSource,
     private val sessionRepository: SessionRepository
 ): AuthenticationRepository {
-    override fun login(username: String, password: String): Result<SessionEntity>{
+    override fun login(username: String, password: String): Result<UserEntity>{
         TODO("Not yet implemented")
     }
 
--- src/main/kotlin/logic/repositories/AuthenticationRepository.kt
+++ src/main/kotlin/logic/repositories/AuthenticationRepository.kt
@@ -1,9 +1,10 @@
 package org.baghdad.logic.repositories
 import org.baghdad.logic.model.entities.SessionEntity
+import org.baghdad.logic.model.entities.UserEntity
 
 interface AuthenticationRepository {
 
-    fun login(username: String, password: String): Result<SessionEntity>
+    fun login(username: String, password: String): Result<UserEntity>
     fun logout(): Result<Unit>
 
 }
