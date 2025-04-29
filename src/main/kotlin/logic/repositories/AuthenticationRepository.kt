58c0e4c3de6dfadc9714bec13d5a5771fce1062a
haider <haideraljawasim@gmail.com>
2025-04-29 18:57:18 +0300

    resolve a conflict when mergeing with develop

--- src/main/kotlin/logic/repositories/AuthenticationRepository.kt
+++ src/main/kotlin/logic/repositories/AuthenticationRepository.kt
@@ -1,8 +1,8 @@
 package org.baghdad.logic.repositories
 
-import org.baghdad.logic.entities.authentication.SessionEntity
+import org.baghdad.logic.model.entities.UserEntity
 
 interface AuthenticationRepository {
-    fun login(username: String, password: String): Result<SessionEntity>
-    fun logout() : Result<Unit>
+
+    fun login(username: String, password: String): UserEntity?
 }
