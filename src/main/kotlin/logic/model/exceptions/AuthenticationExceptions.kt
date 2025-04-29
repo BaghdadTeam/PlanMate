f9226d62841a4fa9b8765f92dccb73d061275a1b
haider <haideraljawasim@gmail.com>
2025-04-30 02:38:04 +0300

    add InvalidCredentialsException for authentication exceptions

--- src/main/kotlin/logic/model/exceptions/AuthenticationExceptions.kt
+++ src/main/kotlin/logic/model/exceptions/AuthenticationExceptions.kt
@@ -4,3 +4,4 @@ class InvalidPasswordException(message: String) : IllegalArgumentException(messa
 class UserCanNotBeFoundException(message: String) : Exception(message)
 class SessionEndedException(message: String) : Exception(message)
 class InvalidSessionException(message: String) : Exception(message)
+class InvalidCredentialsException(message: String) : Exception(message)
