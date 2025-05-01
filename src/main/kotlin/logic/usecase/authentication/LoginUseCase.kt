4574d7002956f39834fef74d61a3d367319647f9
haider <haideraljawasim@gmail.com>
2025-05-01 11:19:20 +0300

    Implement logic for LoginUseCase

--- src/main/kotlin/logic/usecase/authentication/LoginUseCase.kt
+++ src/main/kotlin/logic/usecase/authentication/LoginUseCase.kt
@@ -1,15 +1,33 @@
-package org.baghdad.logic.usecases.authentication
+package org.baghdad.logic.usecase.authentication
 
 import org.baghdad.logic.model.entities.SessionEntity
+import org.baghdad.logic.model.exceptions.InvalidCredentialsException
 import org.baghdad.logic.repositories.AuthenticationRepository
 import org.baghdad.logic.repositories.SessionRepository
+import org.baghdad.logic.repositories.TokenProvider
+import org.baghdad.utils.md5WithSalt
+import java.time.LocalDateTime
 
 class LoginUseCase (
     private val authRepository: AuthenticationRepository,
-    private val sessionRepository: SessionRepository
-)
-{
+    private val sessionRepository: SessionRepository,
+    private val tokenProvider: TokenProvider
+) {
     operator fun invoke(username: String, password: String): Result<SessionEntity> {
-        TODO()
+
+        if (username.isBlank())
+            return Result.failure(InvalidCredentialsException("username must not be blank"))
+        if (password.isBlank())
+            return Result.failure(InvalidCredentialsException("Password must not be empty"))
+        val result = authRepository.login(username, password.md5WithSalt())
+        return result.map { user ->
+            val session = SessionEntity(
+                userId = user.id.toString(),
+                token = tokenProvider.generateToken(),
+                loginTime = LocalDateTime.now(),
+            )
+            sessionRepository.saveSession(session)
+            session
+        }
     }
 }
