ce028c769f49b45966d48442f780f375eb2c61ee
haider <haideraljawasim@gmail.com>
2025-05-01 12:57:16 +0300

    add unit test case for CleanExpiredSessionUseCase

--- src/test/kotlin/logic/usecase/authentication/CleanExpiredSessionUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/authentication/CleanExpiredSessionUseCaseTest.kt
@@ -4,10 +4,12 @@ import helpers.authentication.SessionTestData
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.baghdad.logic.model.entities.SessionEntity
 import org.baghdad.logic.repositories.SessionRepository
 import org.baghdad.logic.usecase.authentication.CleanExpiredSessionUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class CleanExpiredSessionUseCaseTest {
     lateinit var sessionRepository: SessionRepository
@@ -20,12 +22,23 @@ class CleanExpiredSessionUseCaseTest {
     }
     @Test
     fun `Should delete all expired sessions`() {
+        val oldLogin = LocalDateTime.now().minusMinutes(45)
         // Given
-        every { sessionRepository.loadSession() }returns SessionTestData.baseSessionWithExpiredDate
+        every { sessionRepository.loadSession() }returns SessionEntity(
+            userId = "user123",
+            loginTime = oldLogin,
+            token = "token"
+        )
         // When
         useCase.invoke()
         // Then
         verify { sessionRepository.deleteSession() }
+    }
+    @Test
+    fun `Should not delete a session if it does not expired yet`(){
+        every { sessionRepository.loadSession() } returns SessionTestData.baseSession
+        useCase.invoke()
+        verify (exactly = 0){ sessionRepository.deleteSession() }
     }
 
     @Test
