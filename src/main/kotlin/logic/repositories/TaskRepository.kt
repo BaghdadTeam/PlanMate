75a884dc57a0e10100acc5d3d5cfcfa488a51f9a
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-01 23:24:00 +0300

    Refactor TaskRepository interface to update method signatures and add new task retrieval functionality

--- src/main/kotlin/logic/repositories/TaskRepository.kt
+++ src/main/kotlin/logic/repositories/TaskRepository.kt
@@ -4,9 +4,10 @@ import org.baghdad.logic.model.entities.TaskEntity
 
 interface TaskRepository {
 
-    fun createTask(task: TaskEntity): Boolean
+    fun createTask(task: TaskEntity)
     fun getTaskById(id: String): TaskEntity
     fun getTasksByProjectId(id: String): List<TaskEntity>
-    fun updateTask(task: TaskEntity): Boolean
-    fun deleteTask(id: String): Boolean
+    fun getTasksByStateId(id: String): List<TaskEntity>
+    fun updateTask(task: TaskEntity)
+    fun deleteTask(id: String)
 }
