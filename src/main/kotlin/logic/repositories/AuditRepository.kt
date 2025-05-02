9243a113d6fd7882df5e73832f2cbca0ef7c563e
Aboud <gh.abd.com.207@gmail.com>
2025-05-02 05:54:42 -0700

    implements audit repository

--- src/main/kotlin/logic/repositories/AuditRepository.kt
+++ src/main/kotlin/logic/repositories/AuditRepository.kt
@@ -1,11 +1,12 @@
 package org.baghdad.logic.repositories
 
 import org.baghdad.logic.model.entities.AuditEntity
+import java.util.UUID
 
 
 interface AuditRepository {
 
-    fun addAuditEntry(audit: AuditEntity): Boolean
-    fun getAuditByTaskId(taskId: String): List<AuditEntity>?
-    fun getAuditByProjectId(projectId: String): List<AuditEntity>?
+    fun addAuditEntry(audit: AuditEntity)
+    fun getAuditByTaskId(taskId: UUID): List<AuditEntity>
+    fun getAuditByProjectId(projectId: UUID): List<AuditEntity>
 }
