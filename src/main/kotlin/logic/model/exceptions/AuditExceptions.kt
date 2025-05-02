2636c586aab277c11e111ef2875622b5a47d21c0
Aboud <gh.abd.com.207@gmail.com>
2025-05-02 15:15:43 -0700

    delete unused exceptions

---
+++ src/main/kotlin/logic/model/exceptions/AuditExceptions.kt
@@ -0,0 +1,10 @@
+package org.baghdad.logic.model.exceptions
+
+class NoTaskFoundException(message: String) : Exception(message)
+class NoProjectFoundException(message: String) : Exception(message)
+class EmptyActionInAuditEntityException(message: String) : Exception(message)
+class UnSupportedTimeStampFormatException(message: String) : Exception(message)
+
+
+
+
