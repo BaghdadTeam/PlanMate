741830d684e481b1a62e1872c57278169f22125d
Aboud <gh.abd.com.207@gmail.com>
2025-05-02 00:54:40 -0700

    use uuid in ids instead of string

--- src/main/kotlin/logic/model/entities/AuditEntity.kt
+++ src/main/kotlin/logic/model/entities/AuditEntity.kt
@@ -1,12 +1,14 @@
 package org.baghdad.logic.model.entities
 
+
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class AuditEntity(
     val id: UUID = UUID.randomUUID(),
     val entityType: String,  // => Project | State | Task
-    val entityId: String,   // => Project | State | Task
+    val entityId: UUID,   // => Project | State | Task
     val action: String,
     val user: UserEntity,
-    val timestamp: String
+    val timestamp: LocalDateTime = LocalDateTime.now(),
 )
