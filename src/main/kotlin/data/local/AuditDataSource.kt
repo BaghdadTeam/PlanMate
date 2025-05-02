a17af1381d4b361ec59cbdefd9c8f8bfd4adf1ad
Aboud <gh.abd.com.207@gmail.com>
2025-05-02 01:49:06 -0700

    implements AuditDataSource

---
+++ src/main/kotlin/data/local/AuditDataSource.kt
@@ -0,0 +1,32 @@
+package data.local
+
+import org.baghdad.data.datasource.DataSource
+import org.baghdad.logic.model.entities.AuditEntity
+import org.baghdad.logic.model.entities.AuditEntityType
+import org.baghdad.logic.model.exceptions.audit.NoProjectFoundException
+import org.baghdad.logic.model.exceptions.audit.NoTaskFoundException
+import java.util.UUID
+
+
+class AuditDataSource(
+    private val dataSources: DataSource<AuditEntity>
+){
+     fun createAudit(audit: AuditEntity){
+         dataSources.append(audit)
+    }
+
+     fun getAuditByTaskId(taskId: UUID): List<AuditEntity> {
+        return dataSources.loadAll()
+            .filter { (it.entityType == AuditEntityType.Task) && (it.entityId == taskId) }
+            .takeIf { it.isNotEmpty() }
+            ?:throw NoTaskFoundException("No audit found for task with ID: $taskId")
+
+    }
+
+     fun getAuditByProjectId(projectId: UUID): List<AuditEntity> {
+         return dataSources.loadAll()
+             .filter { (it.entityType == AuditEntityType.Project) && (it.entityId == projectId) }
+             .takeIf { it.isNotEmpty() }
+             ?:throw NoProjectFoundException("No audit found for task with ID: $projectId")
+    }
+}
