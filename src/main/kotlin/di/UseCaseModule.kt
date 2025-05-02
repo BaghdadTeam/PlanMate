5f8d9ddb758efe225844b2a788229283a77af64f
Abdelrahman Elgendy <73541296+Abdelrahman-El-Gendy@users.noreply.github.com>
2025-05-02 20:46:13 +0300

    `Adding ProjectsServicesUseCase.kt` to the di module.

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,8 +1,10 @@
 package org.baghdad.di
 
 import org.baghdad.logic.usecase.StateTransitionUseCase
+import org.baghdad.logic.usecase.ProjectsServicesUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { StateTransitionUseCase(get(), get(), get()) }
+    single { ProjectsServicesUseCase(get(), get()) }
 }
