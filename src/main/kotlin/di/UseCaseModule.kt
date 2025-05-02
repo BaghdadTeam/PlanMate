4f11ca39e3c4bb13aecee4f5e39595a41f345319
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-02 14:45:02 +0300

    Add new task-related exceptions and update UseCaseModule for task use cases

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,8 +1,18 @@
 package org.baghdad.di
 
 import org.baghdad.logic.usecase.StateTransitionUseCase
+import org.baghdad.logic.usecase.task.CreateTaskUseCase
+import org.baghdad.logic.usecase.task.GetAllTasksUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
+
+    // region  ::  Task Use Cases  ::
+
     single { StateTransitionUseCase(get(), get(), get()) }
+
+    single { GetAllTasksUseCase(get()) }
+    single { CreateTaskUseCase(get(), get()) }
+
+    // endregion
 }
