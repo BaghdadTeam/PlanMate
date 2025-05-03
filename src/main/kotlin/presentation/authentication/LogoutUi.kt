4ca309db88f62657a52b281a1c0a0f0c4763754a
haider <haideraljawasim@gmail.com>
2025-05-03 22:30:39 +0300

    add catch statments in the Logout UI class

--- src/main/kotlin/presentation/authentication/LogoutUi.kt
+++ src/main/kotlin/presentation/authentication/LogoutUi.kt
@@ -1,8 +1,10 @@
 package org.baghdad.presentation.authentication
 
+import org.baghdad.logic.model.exceptions.LogoutFailedException
 import org.baghdad.logic.usecase.authentication.LogoutUseCase
 import org.baghdad.presentation.input.Reader
 import org.baghdad.presentation.output.Viewer
+import sun.jvm.hotspot.HelloWorld.e
 
 class LogoutUi(
     private val useCase: LogoutUseCase,
@@ -11,11 +13,19 @@ class LogoutUi(
 ) {
     fun execute() {
         viewer.logMessage("Are you sure you want to logout (y)")
-        val userChoice = reader.readInput()?.lowercase()
-        if (userChoice != null ) {
-            if(userChoice == "y") {
-                useCase.invoke()
+        try {
+
+
+            val userChoice = reader.readInput()?.lowercase()
+            if (userChoice != null) {
+                if (userChoice == "y") {
+                    useCase.invoke()
+                }
             }
+        } catch (e: LogoutFailedException) {
+            viewer.logError("Logout failed: ${e.message}")
+        } catch (_: Exception) {
+            viewer.logError("Somthing went wrong")
         }
     }
 }
