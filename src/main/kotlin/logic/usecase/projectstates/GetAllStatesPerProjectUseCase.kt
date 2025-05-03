9328d8b77a0126bb2f22ba3be2ac9f3b9875c101
Narges-Nagy <nargesnagy21@gmail.com>
2025-05-03 09:39:55 +0300

    add GetAllStatesPerProjectUseCase and it's test cases

---
+++ src/main/kotlin/logic/usecase/projectstates/GetAllStatesPerProjectUseCase.kt
@@ -0,0 +1,14 @@
+package org.baghdad.logic.usecase.projectstate
+
+import org.baghdad.logic.model.entities.StateEntity
+import org.baghdad.logic.repositories.ProjectStatesRepository
+
+class GetAllStatesPerProjectUseCase(
+    private val repository: ProjectStatesRepository
+) {
+
+    fun invoke(projectId: String): List<StateEntity>{
+        return repository.getAllStatesPerProject(projectId)
+    }
+
+}
