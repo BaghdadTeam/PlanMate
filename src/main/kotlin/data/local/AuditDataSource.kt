452ed519c9c28127ca24681bcfe1bd3108c08c3f
Aboud <gh.abd.com.207@gmail.com>
2025-05-02 22:35:30 -0700

    update import exception path

--- src/main/kotlin/data/local/AuditDataSource.kt
+++ src/main/kotlin/data/local/AuditDataSource.kt
@@ -3,30 +3,30 @@ package data.local
 import org.baghdad.data.datasource.DataSource
 import org.baghdad.logic.model.entities.AuditEntity
 import org.baghdad.logic.model.entities.Entities
-import org.baghdad.logic.model.exceptions.audit.NoProjectFoundException
-import org.baghdad.logic.model.exceptions.audit.NoTaskFoundException
+import org.baghdad.logic.model.exceptions.NoProjectFoundException
+import org.baghdad.logic.model.exceptions.NoTaskFoundException
 import java.util.UUID
 
 
 class AuditDataSource(
     private val dataSources: DataSource<AuditEntity>
-){
-     fun createAudit(audit: AuditEntity){
-         dataSources.append(audit)
+) {
+    fun createAudit(audit: AuditEntity) {
+        dataSources.append(audit)
     }
 
-     fun getAuditByTaskId(taskId: UUID): List<AuditEntity> {
+    fun getAuditByTaskId(taskId: UUID): List<AuditEntity> {
         return dataSources.loadAll()
             .filter { (it.entityType == Entities.Task) && (it.entityId == taskId) }
             .takeIf { it.isNotEmpty() }
-            ?:throw NoTaskFoundException("No audit found for task with ID: $taskId")
+            ?: throw NoTaskFoundException("No audit found for task with ID: $taskId")
 
     }
 
-     fun getAuditByProjectId(projectId: UUID): List<AuditEntity> {
-         return dataSources.loadAll()
-             .filter { (it.entityType == Entities.Project) && (it.entityId == projectId) }
-             .takeIf { it.isNotEmpty() }
-             ?:throw NoProjectFoundException("No audit found for task with ID: $projectId")
+    fun getAuditByProjectId(projectId: UUID): List<AuditEntity> {
+        return dataSources.loadAll()
+            .filter { (it.entityType == Entities.Project) && (it.entityId == projectId) }
+            .takeIf { it.isNotEmpty() }
+            ?: throw NoProjectFoundException("No audit found for task with ID: $projectId")
     }
 }
