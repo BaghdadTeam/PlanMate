1c0950c98e4c80ba5bcb6ce2e6bcb61793dbee87
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-05 19:11:01 +0300

    refactor: update dummySession to use UUID directly for user ID in DeleteTaskUITest

--- src/test/kotlin/presentation/task/DeleteTaskUITest.kt
+++ src/test/kotlin/presentation/task/DeleteTaskUITest.kt
@@ -24,7 +24,7 @@ class DeleteTaskUITest {
     private lateinit var viewer: Viewer
     private lateinit var reader: Reader
 
-    private val dummySession = SessionEntity(UUID.randomUUID(), UUID.randomUUID().toString(), "", LocalDateTime.now())
+    private val dummySession = SessionEntity(UUID.randomUUID(), UUID.randomUUID(), "", LocalDateTime.now())
     private val task1 = TaskEntity(UUID.randomUUID(), "Task 1", "Description 1", UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID())
     private val task2 = TaskEntity(UUID.randomUUID(), "Task 2", "Description 2", UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID())
 
@@ -47,7 +47,7 @@ class DeleteTaskUITest {
 
         deleteTaskUI.execute(tasks)
 
-        verify { useCase(task1.id, UUID.fromString(dummySession.userId)) }
+        verify { useCase(task1.id, dummySession.userId) }
 
         verify { viewer.logMessage("Task deleted successfully.") }
     }
