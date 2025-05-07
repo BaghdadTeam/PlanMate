e1ea0d7fe434dc124437f9a6fdee3314da88f8b0
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-07 15:46:04 +0300

    feat: remove getTaskById method from TaskRepository interface

--- src/main/kotlin/logic/repositories/TaskRepository.kt
+++ src/main/kotlin/logic/repositories/TaskRepository.kt
@@ -6,7 +6,7 @@ import java.util.UUID
 interface TaskRepository {
 
     fun createTask(task: TaskEntity)
-    fun getTaskById(id: UUID): TaskEntity
+    fun getTaskById(id: UUID): TaskEntity  // Remove this task
     fun getTasksByProjectId(id: UUID): List<TaskEntity>
     fun getTasksByStateId(id: UUID): List<TaskEntity>
     fun updateTask(task: TaskEntity): Boolean
