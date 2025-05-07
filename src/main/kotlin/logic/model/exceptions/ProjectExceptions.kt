a0df6d6ce06980845092c17dd5445ff1b2dce709
Aboud <gh.abd.com.207@gmail.com>
2025-05-07 07:58:56 -0700

    add an exception for AccessDeniedException

--- src/main/kotlin/logic/model/exceptions/ProjectExceptions.kt
+++ src/main/kotlin/logic/model/exceptions/ProjectExceptions.kt
@@ -1,3 +1,5 @@
 package org.baghdad.logic.model.exceptions
 
 class ProjectNotFoundException(message: String) : Exception(message)
+class EmptyProjectNameException(message: String) : IllegalArgumentException(message)
+class AccessDeniedException(message: String) : Exception(message)
