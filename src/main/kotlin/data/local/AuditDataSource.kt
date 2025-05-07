075509bb65967f150fa6a61c1f44fabe7f3bf454
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-07 16:02:29 +0300

    feat: integrate runBlocking for coroutine support in AuditDataSource methods

--- src/main/kotlin/data/local/AuditDataSource.kt
+++ src/main/kotlin/data/local/AuditDataSource.kt
@@ -1,5 +1,6 @@
 package data.local
 
+import kotlinx.coroutines.runBlocking
 import org.baghdad.data.datasource.DataSource
 import org.baghdad.logic.model.entities.AuditLogEntity
 import org.baghdad.logic.model.entities.Entities
@@ -12,21 +13,26 @@ class AuditDataSource(
     private val dataSources: DataSource<AuditLogEntity>
 ) {
     fun createAudit(audit: AuditLogEntity) {
-        dataSources.append(audit)
+        runBlocking {
+            dataSources.append(audit)
+        }
     }
 
     fun getAuditByTaskId(taskId: UUID): List<AuditLogEntity> {
-        return dataSources.loadAll()
-            .filter { (it.entityUnderAudit == Entities.Task.name) && (it.entityId == taskId) }
-            .takeIf { it.isNotEmpty() }
-            ?: throw NoTaskFoundException("No audit found for task with ID: $taskId")
-
+        return runBlocking {
+            dataSources.loadAll()
+                .filter { (it.entityUnderAudit == Entities.Task.name) && (it.entityId == taskId) }
+                .takeIf { it.isNotEmpty() }
+                ?: throw NoTaskFoundException("No audit found for task with ID: $taskId")
+        }
     }
 
     fun getAuditByProjectId(projectId: UUID): List<AuditLogEntity> {
-        return dataSources.loadAll()
-            .filter { (it.entityUnderAudit == Entities.Project.name) && (it.entityId == projectId) }
-            .takeIf { it.isNotEmpty() }
-            ?: throw NoProjectFoundException("No audit found for task with ID: $projectId")
+        return runBlocking {
+            dataSources.loadAll()
+                .filter { (it.entityUnderAudit == Entities.Project.name) && (it.entityId == projectId) }
+                .takeIf { it.isNotEmpty() }
+                ?: throw NoProjectFoundException("No audit found for task with ID: $projectId")
+        }
     }
 }
