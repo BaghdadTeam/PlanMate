c30ab8b55d14d712546dabff2ddaafda62528106
Aboud <gh.abd.com.207@gmail.com>
2025-05-07 20:21:47 -0700

    split the validation into another class, in UserUseCases

--- src/main/kotlin/logic/usecase/user/CreateUserUseCase.kt
+++ src/main/kotlin/logic/usecase/user/CreateUserUseCase.kt
@@ -2,69 +2,25 @@ package org.baghdad.logic.usecase.user
 
 import org.baghdad.logic.model.entities.UserEntity
 import org.baghdad.logic.model.entities.UserType
-import org.baghdad.logic.model.exceptions.user.InvalidNameException
-import org.baghdad.logic.model.exceptions.user.InvalidPasswordException
-import org.baghdad.logic.model.exceptions.user.InvalidUsernameException
-import org.baghdad.logic.model.exceptions.user.UnauthorizedException
-import org.baghdad.logic.model.exceptions.user.UserAlreadyExistsException
-import org.baghdad.logic.model.exceptions.user.UserNotFoundException
 import org.baghdad.logic.repositories.UserRepository
 import org.baghdad.logic.utils.md5WithSalt
+import java.util.UUID
 
 
 class CreateUserUseCase(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val validatorUseCase: UserValidatorUseCase
 ) {
     operator fun invoke(
         username: String,
         passwordPlain: String,
         name: String,
-        creator: UserEntity
+        creatorId: UUID
     ): UserEntity {
-        checkAdmin(creator)
-        validateUsername(username)
-        validateName(name)
-        validatePassword(passwordPlain)
-        ensureUsernameUnique(username)
-
+        validatorUseCase.invoke(username, passwordPlain, name, creatorId)
         val newUser = createUserEntity(username, passwordPlain, name)
         userRepository.createUser(newUser)
-
         return newUser
-    }
-
-    private fun validateUsername(username: String) {
-        if (username.isBlank()) {
-            throw InvalidUsernameException("Username must not be empty.")
-        }
-        val re = Regex("^[A-Za-z0-9_]{3,20}\$")
-        if (!username.matches(re)) {
-            throw InvalidUsernameException(
-                "Username must be 3–20 chars, letters, digits or underscore only."
-            )
-        }
-    }
-
-    private fun checkAdmin(user: UserEntity) {
-        if (user.type != UserType.Admin) {
-            throw UnauthorizedException("Only admins can create users.")
-        }
-    }
-
-    private fun validateName(name: String) {
-        if (name.isBlank()) throw InvalidNameException("Name must not be empty.")
-    }
-
-
-    private fun validatePassword(password: String) {
-        if (password.length < 6) throw InvalidPasswordException("Password must be at least 6 characters.")
-    }
-
-    private fun ensureUsernameUnique(username: String) {
-        try {
-            userRepository.getUserByUsername(username)
-            throw UserAlreadyExistsException("Username '$username' already exists.")
-        } catch (exception: UserNotFoundException) { }
     }
 
     private fun createUserEntity(username: String, password: String, name: String): UserEntity {
