7fbef92c7ba74ae49b0da8e740b0517ffdb305d8
Aboud <gh.abd.com.207@gmail.com>
2025-05-09 09:04:42 -0700

    add statesUis and taskUis to UiModule

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -1,6 +1,8 @@
 package org.baghdad.di
 
+import SwimlaneUI
 import org.baghdad.logic.usecase.projectstates.DeleteStateForProjectUseCase
+import org.baghdad.presentation.StateTransitionUI
 import org.baghdad.presentation.authentication.LoginUi
 import org.baghdad.presentation.project.CreateProjectUi
 import org.baghdad.presentation.project.DeleteProjectUi
@@ -8,14 +10,16 @@ import org.baghdad.presentation.project.EditProjectUi
 import org.baghdad.presentation.project.GetAllProjectsUi
 import org.baghdad.presentation.project.ProjectUi
 import org.baghdad.presentation.projectStates.AddStateToProjectUI
+import org.baghdad.presentation.projectStates.DeleteStateForProjectUI
 import org.baghdad.presentation.projectStates.EditProjectStateUI
 import org.baghdad.presentation.projectStates.GetAllStatesPerProjectUI
 import org.baghdad.presentation.projectStates.GetStateByIdUI
+import org.baghdad.presentation.projectStates.ProjectStatesUI
 import org.baghdad.presentation.swimlane.RenderSwimlaneUI
-import org.baghdad.presentation.swimlane.SwimlaneUI
 import org.baghdad.presentation.task.CreateTaskUI
 import org.baghdad.presentation.task.DeleteTaskUI
 import org.baghdad.presentation.task.GetTasksByStateIdUI
+import org.baghdad.presentation.task.TaskManagementGatherUI
 import org.baghdad.presentation.task.UpdateTaskUI
 import org.baghdad.presentation.user.CreateUserUI
 import org.koin.dsl.module
@@ -56,6 +60,25 @@ val uiModule = module {
 
     // region :: Swimlane ::
     single { RenderSwimlaneUI(get()) }
-    single { SwimlaneUI(get()) }
+    single { SwimlaneUI(get(), get(), get(), get(), get()) }
     // endregion
+
+    //region :: ProjectStates ::
+    single { AddStateToProjectUI(get(), get(), get(), get()) }
+    single { EditProjectStateUI(get(), get(), get(), get()) }
+    single { DeleteStateForProjectUI(get(), get(), get()) }
+    single { GetAllStatesPerProjectUI(get(), get(), get()) }
+    single { ProjectStatesUI(get(), get(), get(), get(), get(), get()) }
+    single { StateTransitionUI(get(), get(), get(), get()) }
+    //endregion
+
+    //region :: Task ::
+    single { CreateTaskUI(get(), get(), get(), get()) }
+    single { DeleteTaskUI(get(), get(), get(), get()) }
+    single { GetTasksByStateIdUI(get(), get(), get()) }
+    single { UpdateTaskUI(get(), get(), get(), get()) }
+    single { TaskManagementGatherUI(get(), get(), get(), get(), get(), get()) }
+    //endregion
+
+
 }
