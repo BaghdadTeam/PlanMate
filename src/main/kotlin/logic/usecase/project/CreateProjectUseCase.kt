4e94479951f49ed3b7497daf54e8d25c1d904fa3
Aboud <gh.abd.com.207@gmail.com>
2025-05-09 04:57:58 -0700

    add audits logs into the project usecases

--- src/main/kotlin/logic/usecase/project/CreateProjectUseCase.kt
+++ src/main/kotlin/logic/usecase/project/CreateProjectUseCase.kt
@@ -1,16 +1,22 @@
 package org.baghdad.logic.usecase.project
 
+import org.baghdad.logic.model.entities.AuditLogEntity
+import org.baghdad.logic.model.entities.Entities
 import org.baghdad.logic.model.entities.ProjectEntity
+import org.baghdad.logic.model.entities.TaskEntity
+import org.baghdad.logic.model.entities.UserEntity
 import org.baghdad.logic.model.entities.UserType
 import org.baghdad.logic.model.exceptions.AccessDeniedException
 import org.baghdad.logic.model.exceptions.EmptyProjectNameException
+import org.baghdad.logic.repositories.AuditRepository
 import org.baghdad.logic.repositories.ProjectRepository
 import org.baghdad.logic.repositories.UserRepository
 import java.util.UUID
 
 class CreateProjectUseCase(
     private val projectRepository: ProjectRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val auditRepository: AuditRepository
 ) {
     suspend operator fun invoke(projectName: String, userId : UUID){
         val user = userRepository.getUserById(userId)
@@ -18,5 +24,21 @@ class CreateProjectUseCase(
         if (projectName.isBlank()) throw EmptyProjectNameException("Project name can't be empty")
         val project = ProjectEntity(name = projectName, creatorId = user.id)
         projectRepository.createProject(project)
+        val audit = logProjectCreation(project, user)
+        auditRepository.addAuditEntry(audit)
     }
+    private fun logProjectCreation(
+        project: ProjectEntity,
+        user: UserEntity
+    ): AuditLogEntity {
+        val action = "created project ${project.name}"
+        val audit = AuditLogEntity(
+            entityUnderAudit = Entities.Project.name,
+            entityId = project.id,
+            action = action,
+            user = user,
+        )
+        return audit
+    }
+
 }
