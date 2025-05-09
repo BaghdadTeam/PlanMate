5d7c90185d848ae14f4088bb01d1965b06a9f132
Aboud <gh.abd.com.207@gmail.com>
2025-05-09 04:53:24 -0700

    implements the main choice in ViewMainManu

---
+++ src/main/kotlin/presentation/app/ViewMainManu.kt
@@ -0,0 +1,39 @@
+package org.baghdad.presentation.app
+
+import org.baghdad.presentation.input.Reader
+import org.baghdad.presentation.output.Viewer
+import org.baghdad.presentation.project.ProjectUi
+import org.baghdad.presentation.swimlane.SwimlaneUI
+import org.baghdad.presentation.user.CreateUserUI
+
+class ViewMainManu(
+    private val projectUi: ProjectUi,
+    private val createUserUI: CreateUserUI,
+    private val viewer: Viewer,
+    private val reader: Reader,
+    private val swimlaneUI: SwimlaneUI
+) {
+    suspend operator fun invoke() {
+        while (true) {
+            viewer.logMessage("=== Main Menu ===")
+            viewer.logMessage("1. View projects")
+            viewer.logMessage("2. Create user")
+            viewer.logMessage("0. Exit")
+            when (reader.readInput()?.toIntOrNull()) {
+                1 -> {
+                    val projectId = projectUi.invoke()
+                    if (projectId == null) {
+                        continue
+                    } else {
+                        swimlaneUI(projectId)
+                    }
+                }
+
+                2 -> createUserUI.invoke()
+                3 -> break
+            }
+
+        }
+    }
+
+}
