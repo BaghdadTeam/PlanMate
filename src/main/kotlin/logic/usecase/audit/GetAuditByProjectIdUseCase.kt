db23395e7bffa38a545926a08e96c11be46a9246
Aboud <gh.abd.com.207@gmail.com>
2025-05-09 13:03:58 -0700

    change the name of method in Audit Repository and Audit dataSource and usecases

--- src/main/kotlin/logic/usecase/audit/GetAuditByProjectIdUseCase.kt
+++ src/main/kotlin/logic/usecase/audit/GetAuditByProjectIdUseCase.kt
@@ -2,13 +2,31 @@ package org.baghdad.logic.usecase.audit
 
 import org.baghdad.logic.model.entities.AuditLogEntity
 import org.baghdad.logic.repositories.AuditRepository
+import org.baghdad.logic.repositories.ProjectStatesRepository
+import org.baghdad.logic.repositories.TaskRepository
 import java.util.UUID
 
 class GetAuditByProjectIdUseCase(
-    val auditRepository: AuditRepository
-){
+    private val auditRepository: AuditRepository,
+    private val projectStatesRepository: ProjectStatesRepository,
+    private val taskRepository: TaskRepository
+) {
     suspend operator fun invoke(projectId: UUID): List<AuditLogEntity> {
-        return auditRepository.getAuditByProjectId(projectId)
+        val projectAudit = auditRepository.getAuditByProjectId(projectId)
+        val tasksIds = taskRepository.getTasksByProjectId(projectId).map { it.id }
+        val statesIds = projectStatesRepository.getAllStatesPerProject(projectId).map { it.id }
+
+        val tasksStateAudit = statesIds.map { stateId ->
+            auditRepository.getAuditByProjectId(stateId)
+        }.flatten()
+
+        val tasksAudit = tasksIds.map { taskId ->
+            auditRepository.getAuditByProjectId(taskId)
+        }.flatten()
+
+        val auditLog = projectAudit + tasksStateAudit + tasksAudit
+
+        return auditLog.sortedByDescending { it.timestamp }
     }
 
 }
