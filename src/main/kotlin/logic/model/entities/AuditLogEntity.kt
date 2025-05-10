f520a48c6aa635148a842204636167b4e0b2c6c1
Aboud <gh.abd.com.207@gmail.com>
2025-05-10 03:17:39 -0700

    set time zone to iraq in auditLogEntity

--- src/main/kotlin/logic/model/entities/AuditLogEntity.kt
+++ src/main/kotlin/logic/model/entities/AuditLogEntity.kt
@@ -3,6 +3,7 @@ package org.baghdad.logic.model.entities
 import java.time.LocalDateTime
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.codecs.pojo.annotations.BsonProperty
+import java.time.ZoneId
 import java.util.UUID
 
 data class AuditLogEntity(
@@ -13,5 +14,5 @@ data class AuditLogEntity(
     val projectId: UUID,
     val action: String,
     val user: UserEntity,
-    val timestamp: LocalDateTime = LocalDateTime.now()
+    val timestamp: LocalDateTime = LocalDateTime.now(ZoneId.of("Asia/Baghdad"))
 ): Identifiable
