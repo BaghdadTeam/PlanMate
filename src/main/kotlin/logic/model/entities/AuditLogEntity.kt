75edfd89156b3bfd714a28f81775953ed8c3e808
Aboud <gh.abd.com.207@gmail.com>
2025-05-11 13:10:06 -0700

    add an action enum class and action property to AuditLogEntity

--- src/main/kotlin/logic/model/entities/AuditLogEntity.kt
+++ src/main/kotlin/logic/model/entities/AuditLogEntity.kt
@@ -7,14 +7,19 @@ import java.time.ZoneId
 import java.util.UUID
 
 data class AuditLogEntity(
-    @BsonId
+
     override val id: UUID = UUID.randomUUID(),
     val entityUnderAudit: String,  // => Project | State | Task
     val entityUnderAuditId : UUID,
-    @BsonProperty("project_id")
+
     val projectId: UUID,
+    val action: Action,
     val description: String,
-    @BsonProperty("user_id")
+
     val userId: UUID,
     val timestamp: LocalDateTime = LocalDateTime.now(ZoneId.of("Asia/Baghdad"))
 ): Identifiable
+
+enum class Action {
+    Delete,Create,Update
+}
