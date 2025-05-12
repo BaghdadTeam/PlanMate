62e21b5fe6613155ba9de8b54ec4be4bcf8a80b5
haider <haideraljawasim@gmail.com>
2025-05-13 01:31:56 +0300

    Implemet the logic of convert from the domian to dto or the opisite

---
+++ src/main/kotlin/data/mapper/UserMapper.kt
@@ -0,0 +1,24 @@
+package org.baghdad.data.mapper
+
+import org.baghdad.data.dto.UserDto
+import org.baghdad.logic.model.entities.UserEntity
+import org.baghdad.logic.model.entities.UserType
+
+
+fun UserDto.toDomain(): UserEntity {
+    return UserEntity(
+        id = this.id,
+        name = this.name,
+        username = this.username,
+        type = UserType.valueOf(this.type)
+    )
+}
+fun UserEntity.toDto(hashedPassword: String): UserDto {
+    return UserDto(
+        id = this.id,
+        username = this.username,
+        name = this.name,
+        hashedPassword = hashedPassword,
+        type = this.type.toString()
+    )
+}
