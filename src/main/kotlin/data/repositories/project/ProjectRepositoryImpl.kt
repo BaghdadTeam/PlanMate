40ee6f294ca85c3bab5c834f83c6ebdd6d50b8d6
Dunya4mars <rudina4mars@gmail.com>
2025-05-12 19:36:39 +0300

    change type dependency  data_source from project_data_source to data_source project_dto type

--- src/main/kotlin/data/repositories/project/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/project/ProjectRepositoryImpl.kt
@@ -1,30 +1,41 @@
 package org.baghdad.data.repositories.project
 
+import org.baghdad.data.datasource.DataSource
+import org.baghdad.data.dto.ProjectDto
 import org.baghdad.data.local.ProjectDataSource
+import org.baghdad.data.mapper.toDto
+import org.baghdad.data.mapper.toEntity
 import org.baghdad.logic.model.entities.ProjectEntity
+import org.baghdad.logic.model.exceptions.ProjectNotFoundException
 import org.baghdad.logic.repositories.ProjectRepository
 import java.util.UUID
 
 class ProjectRepositoryImpl(
-    private val dataSource: ProjectDataSource
+    private val dataSource: DataSource<ProjectDto>
 ) : ProjectRepository {
     override suspend fun createProject(project: ProjectEntity) {
-        dataSource.createProject(project)
+        dataSource.append(project.toDto())
     }
 
     override suspend fun getProjectById(id: UUID): ProjectEntity {
-        return dataSource.getProjectById(id)
+        return dataSource.loadAll()
+            .find { it.id == id }
+            ?.toEntity()
+            ?: throw ProjectNotFoundException("Project with id $id not found")
     }
 
     override suspend fun getAllProjects(): List<ProjectEntity> {
-        return dataSource.getAllProjects()
+        return dataSource.loadAll().map { it.toEntity() }
     }
 
     override suspend fun deleteProject(id: UUID) {
-        dataSource.deleteProject(id)
+        dataSource.loadAll()
+            .find { it.id == id }
+            ?.also { dataSource.delete(it) }
+            ?: throw ProjectNotFoundException("Project with id $id not found")
     }
 
     override suspend fun editProject(project: ProjectEntity) {
-        dataSource.updateProject(project)
+        dataSource.update(project.toDto())
     }
 }
