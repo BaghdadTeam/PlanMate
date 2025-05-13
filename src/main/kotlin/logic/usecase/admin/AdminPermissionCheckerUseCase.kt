ce8d12cab39f4a2bfa69a5e068a4446e5fb78f95
Aboud <gh.abd.com.207@gmail.com>
2025-05-13 03:23:35 -0700

    remove the try catch use case

--- src/main/kotlin/logic/usecase/admin/AdminPermissionCheckerUseCase.kt
+++ src/main/kotlin/logic/usecase/admin/AdminPermissionCheckerUseCase.kt
@@ -2,39 +2,20 @@ package org.baghdad.logic.usecase.admin
 
 import org.baghdad.logic.model.entities.UserType
 import org.baghdad.logic.model.exceptions.user.UnauthorizedException
-import org.baghdad.logic.model.exceptions.user.UserNotFoundException
 import org.baghdad.logic.repositories.UserRepository
 import java.util.UUID
 
 class AdminPermissionCheckerUseCase(
     private val userRepository: UserRepository,
 ) {
-    /**
-     * Checks if user is admin
-     * @return true if admin, false otherwise
-     */
     suspend operator fun invoke(userId: UUID): Boolean {
-        requireNotNull(userId) { "User not found" }
-        return try {
-            validateAdminPermission(userId)
-            true
-        } catch (e: UnauthorizedException) {
-            false
-        }
+        return validateAdminPermission(userId)
+
     }
 
-    /**
-     * Verifies admin status
-     * @throws UnauthorizedException if user is not admin
-     */
-    suspend fun validateAdminPermission(userId: UUID) {
-        val user = userRepository.getUserById(userId) ?: null
-        if (user?.type != UserType.Admin) {
-            throw UnauthorizedException(ADMIN_ONLY_MESSAGE)
-        }
+    suspend fun validateAdminPermission(userId: UUID): Boolean {
+        val user = userRepository.getUserById(userId)
+        return user.type == UserType.Admin
     }
 
-    private companion object {
-        const val ADMIN_ONLY_MESSAGE = "Only admins can perform this action."
-    }
 }
