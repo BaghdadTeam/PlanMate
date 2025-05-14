e6bd1adff39a4dac4b409cb67fade62aec5f8d1e
Aboud-B <78660324+ABDULLAHHG@users.noreply.github.com>
2025-05-14 13:43:17 -0700

    Update ViewMainManu.kt

--- src/main/kotlin/presentation/app/ViewMainManu.kt
+++ src/main/kotlin/presentation/app/ViewMainManu.kt
@@ -27,11 +27,10 @@ class ViewMainManu(
         while (true) {
             viewer.logMessage("=== Main Menu ===")
             viewer.logMessage("1. Project Management")
-            viewer.logMessage("2. Create user")
-            viewer.logMessage("3. Logout")
+            viewer.logMessage("2. Logout")
 
             if (adminPermissionCheckerUseCase(userId)) {
-                viewer.logMessage("2. Create user")
+                viewer.logMessage("3. Create user")
             }
 
             viewer.logMessage("0. Exit")
