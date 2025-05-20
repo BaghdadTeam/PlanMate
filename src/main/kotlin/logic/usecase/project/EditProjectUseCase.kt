ae0678ba545a686aee0070ca1b72e12d75b8728c
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-20 14:31:09 +0300

    refactor: simplify exception handling by removing message from EmptyProjectNameException

--- src/main/kotlin/logic/usecase/project/EditProjectUseCase.kt
+++ src/main/kotlin/logic/usecase/project/EditProjectUseCase.kt
@@ -1,17 +1,15 @@
 package org.baghdad.logic.usecase.project
 
+import org.baghdad.logic.manager.SessionManager
 import org.baghdad.logic.model.entities.Action
 import org.baghdad.logic.model.entities.AuditLogEntity
 import org.baghdad.logic.model.entities.ProjectEntity
-import org.baghdad.logic.manager.SessionManager
 import org.baghdad.logic.model.enums.Entities
-import org.baghdad.logic.model.exceptions.AccessDeniedException
 import org.baghdad.logic.model.exceptions.EmptyProjectNameException
 import org.baghdad.logic.model.exceptions.UnauthorizedException
 import org.baghdad.logic.repositories.AuditRepository
 import org.baghdad.logic.repositories.ProjectRepository
-import org.baghdad.logic.usecase.admin.AdminPermissionCheckerUseCase
-import java.util.UUID
+import java.util.*
 
 class EditProjectUseCase(
     private val projectRepository: ProjectRepository,
@@ -21,7 +19,7 @@ class EditProjectUseCase(
 ) {
     suspend operator fun invoke(projectId: UUID, projectNewName: String, userId: UUID) {
         if (!sessionManager.isAuthenticated()) throw UnauthorizedException()
-        if (projectNewName.isBlank()) throw EmptyProjectNameException("Project name can't be empty")
+        if (projectNewName.isBlank()) throw EmptyProjectNameException()
 
         val existing = projectRepository.getProjectById(projectId)
         val updated = existing.copy(name = projectNewName)
